1594464f5b8409c908fd12afe7c4748ff8be3a27
Magnus Olsson <magnus72@gmail.com>
2012-04-28 16:05:42 +0200

    Added the Strategy/UserDefinedMethods.cs for reusing methods

---
+++ Strategy/UserDefinedMethods.cs
@@ -0,0 +1,20 @@
+#region Using declarations
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using NinjaTrader.Cbi;
+using NinjaTrader.Data;
+using NinjaTrader.Indicator;
+using NinjaTrader.Strategy;
+#endregion
+
+// This namespace holds all strategies and is required. Do not change it.
+namespace NinjaTrader.Strategy
+{
+    /// <summary>
+    /// This file holds all user defined strategy methods.
+    /// </summary>
+    partial class Strategy
+    {
+    }
+}
