dc2289a797168219829d4861b43433fe8a564643
Magnus Olsson <magnus72@gmail.com>
2012-04-28 16:04:03 +0200

    Added the Indicator/UserDefinedMethods.cs file for reusing methods

---
+++ Indicator/UserDefinedMethods.cs
@@ -0,0 +1,19 @@
+#region Using declarations
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using NinjaTrader.Cbi;
+using NinjaTrader.Data;
+using NinjaTrader.Gui.Chart;
+#endregion
+
+// This namespace holds all indicators and is required. Do not change it.
+namespace NinjaTrader.Indicator
+{
+    /// <summary>
+    /// This file holds all user defined indicator methods.
+    /// </summary>
+    partial class Indicator
+    {
+    }
+}
